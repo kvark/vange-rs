699e26bb8f5550c8d39a00e7804d934d01213a82
Dzmitry Malyshau <kvarkus@gmail.com>
2017-04-28 23:32:59 -0400

    New code path for Splay that is faster in Debug

--- lib/splay/src/lib.rs
+++ lib/splay/src/lib.rs
@@ -6,51 +6,68 @@ use byteorder::{LittleEndian as E, ReadBytesExt};
 
 pub struct Splay {
     tree1: [i32; 512],
-    tree1u: [u8; 512],
     tree2: [i32; 512],
-    tree2u: [u8; 512],
 }
 
 impl Splay {
     pub fn new<I: ReadBytesExt>(input: &mut I) -> Splay {
         let mut splay = Splay {
             tree1: [0; 512],
-            tree1u: [0; 512],
             tree2: [0; 512],
-            tree2u: [0; 512],
         };
         for i in 0..512 {
             let v = input.read_i32::<E>().unwrap();
             splay.tree1[i] = v;
-            splay.tree1u[i] = (!v as u8).wrapping_add(1);
         }
         for i in 0..512 {
             let v = input.read_i32::<E>().unwrap();
             splay.tree2[i] = v;
-            splay.tree2u[i] = (!v as u8).wrapping_add(1);
         }
         splay
     }
 
-    fn decompress<I: Read, F: Fn(u8, u8)->u8>(tree: &[i32], tree_char: &[u8],
+    fn decompress<I: Read, F: Fn(u8, u8)->u8>(tree: &[i32],
                   input: &mut I, output: &mut [u8], fun: F) {
-        let mut cur_size = 0;
+        let mut last_char = 0u8;
+        let mut bit = 0;
+        let mut cur = 0u8;
+        for out in output.iter_mut() {
+            let mut code = 1i32;
+            while code > 0 {
+                bit = if bit == 0 {
+                    cur = input.read_u8().unwrap();
+                    7
+                } else {
+                    bit - 1
+                };
+                let i = ((code as usize) << 1) + ((cur >> bit) as usize & 1);
+                code = tree[i];
+            }
+            last_char = fun(last_char, -code as u8);
+            *out = last_char;
+        }
+    }
+
+    #[allow(dead_code)]
+    fn decompress_orig<I: Read, F: Fn(u8, u8)->u8>(tree: &[i32],
+                       input: &mut I, output: &mut [u8], fun: F) {
         let mut last_char = 0u8;
         let mut c_index = 1usize;
+        let mut cur_size = 0;
         loop {
             let cur = input.read_u8().unwrap();
             for bit in (0..8).rev() {
                 let i = (c_index << 1) + ((cur >> bit) as usize & 1);
                 let code = tree[i];
                 c_index = if code <= 0 {
-                    last_char = fun(last_char, tree_char[i]);
+                    last_char = fun(last_char, -code as u8);
                     output[cur_size] = last_char;
                     cur_size += 1;
                     if cur_size == output.len() {
                         return
                     }
                     1
-                }else {
+                } else {
                     code as usize
                 };
             }
@@ -58,9 +75,9 @@ impl Splay {
     }
 
     pub fn expand1<I: Read>(&self, input: &mut I, output: &mut [u8]) {
-        Splay::decompress(&self.tree1, &self.tree1u, input, output, |b, c| b.wrapping_add(c));
+        Splay::decompress(&self.tree1, input, output, |b, c| b.wrapping_add(c));
     }
     pub fn expand2<I: Read>(&self, input: &mut I, output: &mut [u8]) {
-        Splay::decompress(&self.tree2, &self.tree2u, input, output, |b, c| b^c);
+        Splay::decompress(&self.tree2, input, output, |b, c| b^c);
     }
 }
