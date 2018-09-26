75b16520ffd627588748b43fae20195597bc9d7b
Dzmitry Malyshau <kvarkus@gmail.com>
2018-09-25 21:56:40 -0400

    Fix color table level selector

--- data/shader/color.inc.glsl
+++ data/shader/color.inc.glsl
@@ -1,5 +1,8 @@
 // Common FS routines for evaluating terrain color.
 
+//TODO: provide only minimum information to
+// reconstruct the lighting model in shader code.
+
 //uniform sampler2DArray t_Height;
 // This texture has a layer per terrain type. First row is `lightCLR`, second in `palCLR`.
 uniform sampler2DArray t_Table;
@@ -9,16 +12,15 @@ uniform sampler1D t_Palette;
 const float c_HorFactor = 0.5; //H_CORRECTION
 
 vec4 evaluate_color(uint type, vec3 tex_coord, float height_normalized, float lit_factor) {
-    float terrain = float(type) + 0.5;
     float diff =
         textureOffset(t_Height, tex_coord, ivec2(1, 0)).x
         - textureOffset(t_Height, tex_coord, ivec2(-1, 0)).x;
     float light_clr =
-        texture(t_Table, vec3(0.5 * diff + 0.5, 0.25, terrain)).x;
+        texture(t_Table, vec3(0.5 * diff + 0.5, 0.25, float(type))).x;
     float tmp =
         light_clr - c_HorFactor * (1.0 - height_normalized);
     float color_id =
-        texture(t_Table, vec3(0.5 * lit_factor * tmp + 0.5, 0.75, terrain)).x;
+        texture(t_Table, vec3(0.5 * lit_factor * tmp + 0.5, 0.75, float(type))).x;
 
     return texture(t_Palette, color_id);
 }
