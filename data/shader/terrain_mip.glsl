cf44b34ec777e5cc83448b01bb2d765e42576c96
Dzmitry Malyshau <kvarkus@gmail.com>
2019-01-02 21:09:01 -0500

    Non-gather mipmap shader variant

--- data/shader/terrain_mip.glsl
+++ data/shader/terrain_mip.glsl
@@ -1,4 +1,6 @@
+#if GL_ARB_texture_gather
 #extension GL_ARB_texture_gather : enable
+#endif
 
 varying vec3 v_TexCoord;
 
@@ -20,7 +22,19 @@ uniform sampler2DArray t_Height;
 out float Target0;
 
 void main() {
+    #if GL_ARB_texture_gather
     vec4 heights = textureGather(t_Height, v_TexCoord);
+    #else
+    // we are at a pixel center, so a slight offset guarantees
+    // a sample from a particular neighbor in 2x2 grid around the center
+    float delta = 0.00001;
+    vec4 heights = vec4(
+        texture(t_Height, v_TexCoord + vec3(-delta, -delta, 0.0)).r,
+        texture(t_Height, v_TexCoord + vec3(delta, -delta, 0.0)).r,
+        texture(t_Height, v_TexCoord + vec3(delta, delta, 0.0)).r,
+        texture(t_Height, v_TexCoord + vec3(-delta, delta, 0.0)).r
+    );
+    #endif
     Target0 = max(max(heights.x, heights.y), max(heights.z, heights.w));
 }
 #endif //FS
